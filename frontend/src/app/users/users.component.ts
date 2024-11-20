481ddf7d9466857dfe5783b125162b366a8d0fc3
lourdem <60335536+lourdem@users.noreply.github.com>
2024-11-20 10:49:11 -0800

    users page

--- frontend/src/app/users/users.component.ts
+++ frontend/src/app/users/users.component.ts
@@ -1,10 +1,90 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UserService } from 'src/services/user.service';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  users: any[] = [];
+  isAdmin: boolean = false;
+  selectedCompanyId: number | null = null;
+  newUser = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    password: '',
+    confirmPassword: '',
+    admin: '',
+  }
+  showNewUserModal: boolean = false;
 
+  constructor(public http: HttpClient, public userService: UserService, public router: Router) {}
+
+  ngOnInit(): void {
+    const user = this.userService.getUserSession();
+    this.selectedCompanyId = this.userService.getSelectedCompany();
+
+    if (this.selectedCompanyId) {
+      this.isAdmin = user?.role === 'admin';
+      this.fetchUsers(this.selectedCompanyId);
+    } else {
+      console.error('No company selected!');
+    }
+  }
+
+  fetchUsers(companyId: number): void {
+    this.http.get(`http://localhost:8080/company/${companyId}/users`).subscribe({
+      next: (response: any) => {
+        this.users = response;
+      },
+      error: (err) => {
+        console.error('Error fetching users:', err);
+      },
+    });
+  }
+
+  logout() {
+    this.userService.clearSession(); 
+    this.router.navigate(['/login']);
+  }
+
+  openNewUserModal(): void {
+    this.showNewUserModal = true;
+  }
+
+  closeNewUserModal(): void {
+    this.showNewUserModal = false;
+  }
+
+
+createUser(): void {
+  const companyId = this.selectedCompanyId;
+  if (companyId && this.newUser.firstName && this.newUser.lastName && this.newUser.email && this.newUser.password && this.newUser.confirmPassword) {
+    if (this.newUser.password !== this.newUser.confirmPassword) {
+      alert('Passwords do not match.');
+      return;
+    }
+
+    const url = `http://localhost:8080/company/${companyId}/users/create`;
+
+    this.http.post(url, this.newUser).subscribe({
+      next: () => {
+        alert('User created successfully!');
+        this.fetchUsers(companyId);
+        this.closeNewUserModal();
+      },
+      error: (err) => {
+        console.error('Error creating user:', err);
+        alert('Failed to create user.');
+      },
+    });
+  } else {
+    alert('Fill in all user details.');
+  }
+}
 }
